ca0665cdb2b4d73cf33fde68f7aee9c64a3d21b0
Gar|k <garik.djan@gmail.com>
2021-02-16 17:47:14 +0300

    feat: change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ module.exports = function() {
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
-      publicPath: isEnvProduction ? paths.publicUrlOrPath : '/',
+      publicPath: paths.publicUrlOrPath,
       // Prevents conflicts when multiple webpack runtimes (from different apps)
       // are used on the same page.
       jsonpFunction: `webpackJsonp${appPackageJson.name}`,
@@ -364,7 +364,7 @@ module.exports = function() {
           {},
           {
             // For iframe relative paths
-            publicPath: '/',
+            publicPath: paths.publicUrlOrPath,
             inject: true,
             template: paths.appHtml,
           },
